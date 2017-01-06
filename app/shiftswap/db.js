dacc707a7b91df234880b1444465a4501d2941ab
Iuykza <adijkstra94@gmail.com>
2017-01-06 01:52:29 -0600

    Renamed get to find, added default, query fix
    Renamed get to find for db consistency.
    Added a default 7 day get if nothing was passed.
    Fixed query to return start<=y<=end instead of start<y<end.

--- app/shiftswap/db.js
+++ app/shiftswap/db.js
@@ -11,23 +11,46 @@ serverConfig = require('app/_config/server.json')
 var model = schema.model;
 
 exports.schedule = {
-    get: (start, end, get, callback)=>{
+    find: (start, end, get, callback)=>{
         get   = get || {};
         start = parse.date.unix(start);
         end   = parse.date.unix(end  );
 
+
+        //default to 7 days
+        if(!end){
+            end = -7;
+        }
+
+        //negative values are x days forward.
+        if(end < 0){
+            var secondsInDay = 60 * 60 * 24;
+            end = start + (secondsInDay*Math.abs(end));
+        }
+
         query = {$and: [
-            {'date.unix': {$gt: start}},
-            {'date.unix': {$lt: end}},
+            {'date.unix': {$gte: start}},
+            {'date.unix': {$lte: end}},
             get,
         ]};
 
+        console.log(JSON.stringify(query));
+
         model.Schedule.find(query, function(err, data){
-            if(callback){ callback(err,data);}
+            console.log(data, callback);
+            if(callback){
+                callback(err, data);
+            }
         });
     },
     getFloor:   (start, end, callback)=>{ getAll(start, end, {access: 'f'}, callback) },
     getManager: (start, end, callback)=>{ getAll(start, end, {access: 'm'}, callback) },
+    insert: (data,callback)=>{
+        new model.Schedule(data).save(()=>{
+            if(callback)
+                callback();
+        });
+    }
 };
 exports.users = {
     getUser: (query, callback)=>{
