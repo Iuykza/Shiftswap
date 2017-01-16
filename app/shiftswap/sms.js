0a903e002d53e6979217b75e622ba0ac7027b769
Iuykza <adijkstra94@gmail.com>
2017-01-15 21:17:25 -0600

    detects who the user is by number

--- app/shiftswap/sms.js
+++ app/shiftswap/sms.js
@@ -15,79 +15,103 @@ twilio  = require('twilio')(config.id, config.token)
 exports.parseIncoming = (req, res, db, parse, get)=>{
     get = get || {};
 
-    var number   = get.number;
-    var uid      = db.getUserByPhone(number);
-    var body     = S(get.body).toLowerCase().collapseWhitespace().s;
-    var commands = [
-        {
-            name:'echo',
-            f: ()=>{
-                exports.send(number, body);
-            },
-        },
-        {
-            name:'time',
-            f: ()=>{
-                var m = moment.utc();
-                body = JSON.stringify({
-                    human:   m.format('dddd, MMMM Do YYYY, h:mm:ss a'),
-                    unix:    m.valueOf(),
-                    iso8601: m.toISOString(),
-                });
-                exports.send(number, body);
-            },  
-        },
-        {
-            name:'today',
-            alias:['now'],
-            f: ()=>{
-                var user     = db.getUserByPhone(number);
-                var schedule = db.getSchedule(user, 'today');
-                var msg      = parse.schedule.pretty(schedule);
-                exports.send(number, body);
-            },
-        },
-        {
-            name:'alarm',
-            val:['alarm','warn','remind'],
-            f: ()=>{
+    var number = get.number;
 
-            },
-        },
-        {
-            name:'break',
-            f: ()=>{
-
-            },
-        },
-        {
-            name:'end',
-            alias:['end','out','finish','done'],
-            f: ()=>{
-
-            },
-        },
-        {
-            name: 'shifts',
-            f: ()=>{
-
-            },
+    db.users.getByPhone(number, callbackUID);
+    function callbackUID(err, uid){
+        if(err){
+            return console.error(err);
         }
-    ];
 
-    //Match their text to the list of commands above
-    for(var i=commands.length; i--;){
-        var command = commands[i];
-        if(S(body).contains(command.name)){
-            body = S(body).chompLeft(command.name+' ').s;
-            params = body.split(' ');
-            command.f(params);
-            return;
+        var body     = S(get.body).toLowerCase().collapseWhitespace().s;
+        var found  = false;
+
+        if(!uid){
+            return exports.send(number, 'Who are you?  Reply back with "This is ..."');
         }
+
+        var commands = [
+            {
+                name:'echo',
+                f: ()=>{
+                    exports.send(number, body);
+                },
+            },
+            {
+                name:'time',
+                alias:['date','clock'],
+                f: ()=>{
+                    var m = moment.utc();
+                    body = JSON.stringify({
+                        human:   m.format('dddd, MMMM Do YYYY, h:mm:ss a'),
+                        unix:    m.valueOf(),
+                        iso8601: m.toISOString(),
+                    });
+                    exports.send(number, body);
+                },  
+            },
+            {
+                name:'today',
+                alias:['now'],
+                f: ()=>{
+                    //First get schedule by UID
+                    var today = parse.date.unixToday();
+                    db.schedule.getByUID(today, today, uid, function(err, schedule){
+                        if(err)
+                            return console.error(err);
+
+                        schedule = schedule[0]; //grab only the first schedule
+                        body = parse.schedule.pretty(schedule); //make it pretty
+                        exports.send(number, body); //text it
+                    });
+                },
+            },
+            {
+                name:'alarm',
+                val:['alarm','warn','remind'],
+                f: ()=>{
+
+                },
+            },
+            {
+                name:'break',
+                f: ()=>{
+
+                },
+            },
+            {
+                name:'end',
+                alias:['end','out','finish','done'],
+                f: ()=>{
+
+                },
+            },
+            {
+                name: 'shifts',
+                f: ()=>{
+
+                },
+            }
+        ];
+
+        //Match their text to the list of commands above
+        for(var i=commands.length; i--;){
+            var command = commands[i];
+            if(S(body).contains(command.name)){
+                body = S(body).chompLeft(command.name+' ').s;
+                params = body.split(' ');
+                command.f(params);
+                found = true;
+            }
+        }
+        if(found){
+            return body;
+        }
+
+        //Reaching this point means nothing matched
+        body = "Sorry didn't understand that.  For a list of commands type help.  For help on a specific command type the command and help.";
+        exports.send(number, body);
     }
-    //Reaching this point means nothing matched
-    body = "Sorry didn't understand that.  For a list of commands type help.  For help on a specific command type the command and help.";
-    exports.send(number, body);
 
 };
 exports.send = (toNumber, msg, callback)=>{
